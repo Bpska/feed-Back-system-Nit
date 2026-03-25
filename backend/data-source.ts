1e384238d9c73ef96f3cc5dd10a7880182f0a9ee
Bpska <bpskar2@gmail.com>
2026-03-25 09:27:44 +0530

    Fix internal DB port

--- backend/data-source.ts
+++ backend/data-source.ts
@@ -10,7 +10,7 @@ const configService = new ConfigService();
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: configService.get('DATABASE_HOST'),
-  port: configService.get('DATABASE_PORT') ? parseInt(configService.get('DATABASE_PORT')!) : 5423,
+  port: configService.get('DATABASE_PORT') ? parseInt(configService.get('DATABASE_PORT')!) : 5432,
   username: configService.get('DATABASE_USERNAME'),
   password: configService.get('DATABASE_PASSWORD'),
   database: configService.get('DATABASE_NAME'),
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,7 +17,7 @@ import * as entities from './entities';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT || '5423'),
+      port: parseInt(process.env.DATABASE_PORT || '5432'),
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
